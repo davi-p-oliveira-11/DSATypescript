c77bd461a2dd1a50e6cbc2252ef422365fe15a24
Davi P. Oliveira <dpo.dev11@gmail.com>
2025-07-28 15:11:14 -0300

    added intro and index.ts file for array

---
+++ 03-Arrays/1-Intro/index.ts
@@ -0,0 +1,9 @@
+const stringArray: string[] = ["a", "b", "c", "d"];
+const numberArray: number[] = [1, 2, 3, 4, 5];
+const booleanArray: boolean[] = [true, false];
+const mixedArray: any[] = ["a", 2, true, undefined, null, { a: "a" }, ["b"]];
+
+console.log(stringArray);
+console.log(numberArray);
+console.log(booleanArray)
+console.log(mixedArray);
