b896e5d7793fc084d965a5fd8641eec2629d08a1
Davi P. Oliveira <dpo.dev11@gmail.com>
2025-08-26 17:50:00 -0300

    added merge sort

---
+++ 09-Sorting-Algo/insertion-sort.ts
@@ -0,0 +1,19 @@
+function insertionSort(arr: number[]): number[] {
+  for (let i = 1; i < arr.length; i++) {
+    const key = arr[i];
+    let j = i - 1;
+
+    while (j >= 0 && arr[j] > key) {
+      arr[j + 1] = arr[j];
+      j--;
+    }
+
+    arr[j + 1] = key;
+  }
+
+  return arr;
+}
+
+const unsortedArray: number[] = [5, 2, 4, 6, 1, 3];
+const sortedArray: number[] = insertionSort(unsortedArray);
+console.log(sortedArray); // [1, 2, 3, 4, 5, 6]
